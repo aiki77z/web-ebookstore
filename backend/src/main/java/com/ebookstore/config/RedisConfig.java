772938dd1e781cbfa4fe78cef56ebc07576620ac
aiki77z <2434083975@qq.com>
2025-10-29 16:06:20 +0800

    大三上hw4,重构数据库，增加redis

--- backend/src/main/java/com/ebookstore/config/RedisConfig.java
+++ backend/src/main/java/com/ebookstore/config/RedisConfig.java
@@ -3,20 +3,42 @@ package com.ebookstore.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.util.StringUtils;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        String host = (redisProperties.getHost() == null || redisProperties.getHost().isEmpty())
+                ? "localhost" : redisProperties.getHost();
+        int port = (redisProperties.getPort() <= 0) ? 6379 : redisProperties.getPort();
+
+        RedisStandaloneConfiguration standalone = new RedisStandaloneConfiguration(host, port);
+        if (StringUtils.hasText(redisProperties.getPassword())) {
+            standalone.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        }
+
+        Duration cmdTimeout = redisProperties.getTimeout() != null ? redisProperties.getTimeout() : Duration.ofSeconds(1);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(cmdTimeout)
+                .shutdownTimeout(Duration.ofMillis(100))
+                .build();
+
+        return new LettuceConnectionFactory(standalone, clientConfig);
     }
 
     @Bean
